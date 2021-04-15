a4d2594bfc08cb2feb428161b3ab18f19abca9ec
Anderson Y. Torres Q <torandersonq@gmail.com>
2021-04-15 16:25:23 -0500

    Se modifica clase 4.m - agregando informacion sobre teoria funciones

--- Clase_4/clase_4.m
+++ Clase_4/clase_4.m
@@ -1,1 +1,31 @@
+% En la clase 4 se realiza todo lo referente a funciones:
 
+% Las funciones se crean del mismo modo que un script seleccionado en el menú New/Function y se guardan en un fichero que tiene el mismo nombre que la función y extensión .m
+% Su estructura se describe de la siquiente forma:
+%   function variables_salida=nombre_funcion(variables_entrada)
+%      sentencias
+%   end
+
+% Tipos para declarar una funcion:
+%   - Cuando una función no devuelve ningún valor, se define
+%         function nombre_funcion(variables_entrada)
+%   - Cuando una funcion no precisa de variables de entrada se define
+%         function variables_salida=nombre_funcion
+%   - Cuando una función devuelve una solo resultado y se puede escribir, de dos maneras
+%         function [y]=nombre_funcion(a,b,c)
+%         function y=nombre_funcion(a,b,c)
+%   - En general, definiremos una función del siguiente modo:
+%         function [y1,y2]=nombre_funcion(a,b,c)
+%      con tres argumentos a, b y c variables de entrada y que devuelve dos
+%      resultados en las variables y1 e y2.
+
+% Una función se llama del mismo modo que las funciones predefinidas. Las funciones se pueden llamar desde la ventana de comandos, desde un fichero script o desde otra función.
+
+% Todas las variables en una función son locales a dicha función, incluyendo las de entrada y las de salida. 
+
+% Ejemplo:
+   function [z] = suma (x,y)
+   %Esta función suma dos números x & y
+   %y devuelve el resultado de la suma en z
+      z=x+y; %efectúa la suma
+   end
--- Clase_4/vecto1.m
+++ Clase_4/vecto1.m
@@ -1,13 +1,13 @@
 function [G] = vecto1(h,k,x,y,z)
 % Sean los escalares h =0.71 y k=8.852, y los vectores x=[1 2 3 4 5], y=[2.1 2 1.9 1.8 1.7] y z=[2 2.5 3 3.5 4]
-% Resuelva la siguiente ecuacion
+% Resuelva la siguiente ecuacion G=(h.*x+k.*y)./(2.*(x+y).^h) + (3*exp(h.*y./2.*z))./(z.^(0.5.*y./x)) - sqrt(abs((x-y)./2.*z))
 h=0.71; % Valor de h
 k=8.852; % Valor de k
 x=input('Digite el o los valores de x: '); % Solicita Valor(es) de X
 y=input('Digite el o los valores de y: '); % Solicita Valor(es) de Y
 z=input('Digite el o los valores de z: '); % Solicita Valor(es) de Z
-G=(h.*x+k.*y)./(2.*(x+y).^h) + (3*exp(h.*y./2.*z))./(z.^(0.5.*y./x)) - sqrt(abs((x-y)./2.*z));
+G=(h.*x+k.*y)./(2.*(x+y).^h) + (3*exp(h.*y./2.*z))./(z.^(0.5.*y./x)) - sqrt(abs((x-y)./2.*z)); %% Operacion funcion
 disp('el valor de G es: ');
-disp(G);
+disp(G); % Impresion resultado
 end
 
