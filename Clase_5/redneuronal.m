54f19b4c24d50237b3e8400d4adf3de7d92bf305
Anderson Y. Torres Q <torandersonq@gmail.com>
2021-04-15 15:45:43 -0500

    Se crea un funcion vecto1 y se inicia con los fundamentos de una red neuronal

---
+++ Clase_5/redneuronal.m
@@ -0,0 +1,13 @@
+%% Inapsis 
+%%
+x=[1 3 5]; %Vector de entrada
+w=magic(3);%Matriz de pesos sinapticos exitativo
+y=x*w;
+%%
+x1=[-2 3];
+w1=[-3,2,0;1 4 -6];
+y1=x1*w1;
+b1=2;   
+y2=y1-b1
+surf(w2)
+xlabel('x'),ylabel('y'),zlabel('z'), grid on
