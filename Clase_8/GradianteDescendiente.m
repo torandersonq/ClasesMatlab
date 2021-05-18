dceb70ecfaf09e1ff1514e7a71291e2d6fd8415f
Anderson Y. Torres Q <torandersonq@gmail.com>
2021-05-18 15:00:18 -0500

    Se agrega ejercicios sobre Electrontios

--- Clase_8/GradianteDescendiente.m
+++ Clase_8/GradianteDescendiente.m
@@ -15,16 +15,83 @@ end
 movie(mov,10)
 pause(1)
 %% Determinar la energia en Eletronvoltios de las siguiente ondas EM 1. Rayos X 15 nm
-lamda = 15e-9; % metros
+clear all, clc;
+format long g;
+lamda = 15e-9;
+h = 6.626069e-34; % Joules x segundos, constante de planck CONSTANTE
+c = 299792458; % metros/segundos, que es la velocidad de la luz CONSTANTE
+% E  = h * f
+% c = lamda * f --> f = c/lamda
+% Reemplazo f   E = h * (c / lamda)
+E = h *(c/lamda) % Respuesta en joules
+eV = 1.6e-19; % Valor de 1 joules CONSTANTE
+E = E / eV % Respuesta en electonvoltios (unidad de energia)
+%% Determinar la energia en Eletronvoltios de las siguiente ondas EM 2. Microondas 17 GHz
+clear all, clc;
+format long g;
+lamda = 17e9;
+h = 6.626069e-34; % Joules x segundos, constante de planck CONSTANTE
+c = 299792458; % metros/segundos, que es la velocidad de la luz CONSTANTE
+% E  = h * f
+% c = lamda * f --> f = c/lamda
+% Reemplazo f   E = h * (c / lamda)
+E = h *(c/lamda) % Respuesta en joules
+eV = 1.6e-19; % Valor de 1 joules CONSTANTE
+E = E / eV % Respuesta en electonvoltios (unidad de energia)
+%% Determinar la energia en Eletronvoltios de las siguiente ondas EM 3. Espectro Visible 500 THZ
+clear all, clc;
+format long g;
+lamda = 500e12;
+h = 6.626069e-34; % Joules x segundos, constante de planck CONSTANTE
+c = 299792458; % metros/segundos, que es la velocidad de la luz CONSTANTE
+% E  = h * f
+% c = lamda * f --> f = c/lamda
+% Reemplazo f   E = h * (c / lamda)
+E = h *(c/lamda) % Respuesta en joules
+eV = 1.6e-19; % Valor de 1 joules CONSTANTE
+E = E / eV % Respuesta en electonvoltios (unidad de energia)
+%% Determinar la energia en Eletronvoltios de las siguiente ondas EM 4. Espectro Visible 2500 Hz
+lamda = 2500e2; % hecto
 h = 6.626069e-34; % Joules x segundos, constante de planck
-% E = h * f
+% E  = h * f
 % c = lamda * f --> f = c/lamda
 c = 3e8; % metros/segundos, que es la velocidad de la luz
 E = h * c * lamda; % Respuesta en joules
 eV = 1.6e-19; % Valor de 1 joules
 E = E / eV % Respuesta en electonvoltios (unidad de energia)
 %% Si la energia de una onda EM = 41.413 MeV (mega electron voltios), cual es la longitud de onda y de que tipo se trata
+clear all, clc;
 E = 41.413e6; % MegaeV
+eV = 1.6e-19; % Valor de 1 joules
+E = E * eV; 
+% E = h*c/lamda
+h = 6.626069e-34; % Joules x segundos, constante de planck
+c = 3e8; % metros/segundos, que es la velocidad de la luz
+lamda = h * c / E % Respuesta en metros
+%% Si la energia de una onda EM = 0.5 MeV (mega electron voltios), cual es la longitud de onda y de que tipo se trata
+clear all, clc;
+E = 0.5e6; % MegaeV
+eV = 1.6e-19; % Valor de 1 joules
+E = E * eV; 
+% E = h*c/lamda
+h = 6.626069e-34; % Joules x segundos, constante de planck
+c = 3e8; % metros/segundos, que es la velocidad de la luz
+lamda = h * c / E % Respuesta en metros
+% Respuesta rayos gamma =      2.4848e-12
+%% Si la energia de una onda EM = 4.5KeV (kilo electron voltios), cual es la longitud de onda y de que tipo se trata
+clear all, clc;
+E = 4.5e3; % KiloeV
+eV = 1.6e-19; % Valor de 1 joules
+E = E * eV; 
+% E = h*c/lamda
+h = 6.626069e-34; % Joules x segundos, constante de planck
+c = 3e8; % metros/segundos, que es la velocidad de la luz
+lamda = h * c / E % Respuesta en metros
+% Respuesta rayos gamma =    2.7609e-10
+%% Si la energia de una onda EM = 0.7 meV (mili electron voltios), cual es la longitud de onda y de que tipo se trata
+clear all, clc;
+format 
+E = 0.7e-3; % milieV
 eV = 1.6e-19; % Valor de 1 joules
 E = E * eV; 
 % E = h*c/lamda
